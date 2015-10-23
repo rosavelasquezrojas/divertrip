38874d5d702f1a1b8e3163f6e8118dc0473f38f7
rosavelasquezrojas <rvelasquez.rojas@gmail.com>
2015-10-23 03:07:30 -0200

    Backups Base de datos

--- protected/data/divertrip.sql
+++ protected/data/divertrip.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-10-2015 a las 16:57:46
+-- Tiempo de generación: 23-10-2015 a las 06:48:49
 -- Versión del servidor: 5.6.21-log
 -- Versión de PHP: 5.5.12
 
@@ -35,14 +35,6 @@ CREATE TABLE IF NOT EXISTS `administrador` (
   KEY `fk_Administrador_Login1_idx` (`Login_user_name`,`Login_password`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `administrador`
---
-
-INSERT INTO `administrador` (`idAdministrador`, `last_access`, `Login_user_name`, `Login_password`) VALUES
-(1, '2015-10-03 00:00:00', 'admin1', 'admin1'),
-(10, '2015-10-03 00:00:00', 'admin1', 'a1');
-
 -- --------------------------------------------------------
 
 --
@@ -55,14 +47,6 @@ CREATE TABLE IF NOT EXISTS `categoria` (
   `description` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idCategoria`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `categoria`
---
-
-INSERT INTO `categoria` (`idCategoria`, `name`, `description`) VALUES
-(1, 'Reggae', 'divertido'),
-(2, 'Cumbia', 'buenita');
 
 -- --------------------------------------------------------
 
@@ -77,35 +61,17 @@ CREATE TABLE IF NOT EXISTS `direccion` (
   PRIMARY KEY (`idDireccion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `direccion`
---
-
-INSERT INTO `direccion` (`idDireccion`, `latitud`, `longitud`) VALUES
-(1, '-33.0525828', '-71.609887');
-
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `estado_patrocinador`
+-- Estructura de tabla para la tabla `estado`
 --
 
-CREATE TABLE IF NOT EXISTS `estado_patrocinador` (
-  `idEstado_Patrocinador` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `estado` (
+  `idEstado` int(11) NOT NULL,
   `description` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idEstado_Patrocinador`)
+  PRIMARY KEY (`idEstado`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `estado_patrocinador`
---
-
-INSERT INTO `estado_patrocinador` (`idEstado_Patrocinador`, `description`) VALUES
-(1, 'activo'),
-(2, 'inactivo'),
-(3, 'activo'),
-(4, 'inactivo'),
-(10, 'activo');
 
 -- --------------------------------------------------------
 
@@ -117,44 +83,49 @@ CREATE TABLE IF NOT EXISTS `evento` (
   `idEvento` int(11) NOT NULL,
   `name_event` varchar(45) DEFAULT NULL,
   `description_event` varchar(45) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `hour` time DEFAULT NULL,
   `Categoria_idCategoria` int(11) NOT NULL,
   `Direccion_idDireccion` int(11) NOT NULL,
   `Patrocinador_idPatrocinador` int(11) NOT NULL,
-  `fecha` date DEFAULT NULL,
-  `hora` time DEFAULT NULL,
+  `image` text,
   PRIMARY KEY (`idEvento`),
   KEY `fk_Eventos_Categoria1_idx` (`Categoria_idCategoria`),
   KEY `fk_Eventos_Direccion1_idx` (`Direccion_idDireccion`),
   KEY `fk_Eventos_Patrocinador1_idx` (`Patrocinador_idPatrocinador`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `evento`
+-- Estructura de tabla para la tabla `filtro_usuario`
 --
 
-INSERT INTO `evento` (`idEvento`, `name_event`, `description_event`, `Categoria_idCategoria`, `Direccion_idDireccion`, `Patrocinador_idPatrocinador`, `fecha`, `hora`) VALUES
-(1, 'Shamanes', 'prueba1', 1, 1, 10, '2015-10-23', NULL),
-(10, 'Shamanes', 'prueba1', 1, 1, 10, '2015-10-23', NULL),
-(11, 'Movimiento original', 'dsahjas', 1, 1, 1, '2015-10-30', NULL),
-(12, 'March Antony', 'concierto', 2, 1, 10, '2015-10-26', '19:00:00');
+CREATE TABLE IF NOT EXISTS `filtro_usuario` (
+  `Usuario_idUsuario` int(11) NOT NULL,
+  `Categoria_idCategoria` int(11) NOT NULL,
+  PRIMARY KEY (`Categoria_idCategoria`,`Usuario_idUsuario`),
+  KEY `fk_Filto_Usuario_Usuario1_idx` (`Usuario_idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `filtro`
+-- Estructura de tabla para la tabla `ha_logins`
 --
 
-CREATE TABLE IF NOT EXISTS `filtro` (
-  `idFiltro` int(11) NOT NULL,
-  `Filtrocol1` tinyint(1) NOT NULL,
-  `Filtrocol2` tinyint(1) NOT NULL,
-  `Filtrocol3` tinyint(1) NOT NULL,
-  `Filtrocol4` tinyint(1) NOT NULL,
-  `Filtrocol5` tinyint(1) NOT NULL,
-  `Filtrocol6` tinyint(1) NOT NULL,
-  `Filtrocol7` tinyint(1) NOT NULL,
-  PRIMARY KEY (`idFiltro`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `ha_logins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `loginProvider` varchar(50) NOT NULL,
+  `loginProviderIdentifier` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `loginProvider_2` (`loginProvider`,`loginProviderIdentifier`),
+  KEY `loginProvider` (`loginProvider`),
+  KEY `loginProviderIdentifier` (`loginProviderIdentifier`),
+  KEY `userId` (`userId`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -167,16 +138,6 @@ CREATE TABLE IF NOT EXISTS `login` (
   `password` varchar(10) NOT NULL,
   PRIMARY KEY (`user_name`,`password`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `login`
---
-
-INSERT INTO `login` (`user_name`, `password`) VALUES
-('admin1', 'a1'),
-('admin1', 'admin1'),
-('admin2', 'a2'),
-('admin2', 'admin2');
 
 -- --------------------------------------------------------
 
@@ -206,19 +167,11 @@ CREATE TABLE IF NOT EXISTS `patrocinador` (
   `last_name` varchar(45) DEFAULT NULL,
   `Login_user_name` varchar(10) NOT NULL,
   `Login_password` varchar(10) NOT NULL,
-  `Estado_Patrocinador_idEstado_Patrocinador` int(11) NOT NULL,
-  PRIMARY KEY (`idPatrocinador`),
+  `Estado_idEstado` int(11) NOT NULL,
+  PRIMARY KEY (`idPatrocinador`,`Estado_idEstado`),
   KEY `fk_Patrocinador_Login_idx` (`Login_user_name`,`Login_password`),
-  KEY `fk_Patrocinador_Estado_Patrocinador1_idx` (`Estado_Patrocinador_idEstado_Patrocinador`)
+  KEY `fk_Patrocinador_Estado1_idx` (`Estado_idEstado`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `patrocinador`
---
-
-INSERT INTO `patrocinador` (`idPatrocinador`, `first_name`, `last_name`, `Login_user_name`, `Login_password`, `Estado_Patrocinador_idEstado_Patrocinador`) VALUES
-(1, 'pat1', 'pat1', 'admin1', 'a1', 1),
-(10, 'pat1', 'pat1', 'admin1', 'a1', 1);
 
 -- --------------------------------------------------------
 
@@ -230,9 +183,7 @@ CREATE TABLE IF NOT EXISTS `preferencia` (
   `idPreferencia` int(11) NOT NULL,
   `language` varchar(45) DEFAULT NULL,
   `notification` tinyint(1) DEFAULT NULL,
-  `Filtro_idFiltro` int(11) NOT NULL,
-  PRIMARY KEY (`idPreferencia`),
-  KEY `fk_Preferencia_Filtro1_idx` (`Filtro_idFiltro`)
+  PRIMARY KEY (`idPreferencia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -287,6 +238,13 @@ ALTER TABLE `evento`
   ADD CONSTRAINT `fk_Eventos_Patrocinador1` FOREIGN KEY (`Patrocinador_idPatrocinador`) REFERENCES `patrocinador` (`idPatrocinador`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
+-- Filtros para la tabla `filtro_usuario`
+--
+ALTER TABLE `filtro_usuario`
+  ADD CONSTRAINT `fk_Filto_Usuario_Categoria1` FOREIGN KEY (`Categoria_idCategoria`) REFERENCES `categoria` (`idCategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Filto_Usuario_Usuario1` FOREIGN KEY (`Usuario_idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
 -- Filtros para la tabla `lugar_emblematico`
 --
 ALTER TABLE `lugar_emblematico`
@@ -296,14 +254,8 @@ ALTER TABLE `lugar_emblematico`
 -- Filtros para la tabla `patrocinador`
 --
 ALTER TABLE `patrocinador`
-  ADD CONSTRAINT `fk_Patrocinador_Estado_Patrocinador1` FOREIGN KEY (`Estado_Patrocinador_idEstado_Patrocinador`) REFERENCES `estado_patrocinador` (`idEstado_Patrocinador`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Patrocinador_Estado1` FOREIGN KEY (`Estado_idEstado`) REFERENCES `estado` (`idEstado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_Patrocinador_Login` FOREIGN KEY (`Login_user_name`, `Login_password`) REFERENCES `login` (`user_name`, `password`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `preferencia`
---
-ALTER TABLE `preferencia`
-  ADD CONSTRAINT `fk_Preferencia_Filtro1` FOREIGN KEY (`Filtro_idFiltro`) REFERENCES `filtro` (`idFiltro`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `usuario`
